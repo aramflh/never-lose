356c5951a82b91c8054bdacc544a65fa5e509bfa
Aramson <82621068+aramflh@users.noreply.github.com>
2023-05-10 17:10:50 +0200

    A

--- NeverLose.cydsn/main.c
+++ NeverLose.cydsn/main.c
@@ -11,9 +11,6 @@ CY_ISR (myISR){
         // Place ISR code here
 }
 
-// Global vaiables
-uint8_t score = 0;
-
 int main(void)
 {
     CyGlobalIntEnable;  /* Enable global interrupts. */
@@ -27,10 +24,12 @@ int main(void)
 
     // Variables initialization
 
-    uint8_t sw3_pressed = 0;  // Reset button
-
-    // Checking buttons states
-
+    uint8_t score = 0;
+    uint8_t first_jump = 1;
+    uint8_t day = 0;
+    uint8_t night = 0;
+    uint32_t photoResVal1 = 0;
+    uint32_t photoResVal2 = 0;
     char key = 'z';
 
 
@@ -39,70 +38,126 @@ int main(void)
     {
         /* Place your application code here. */
 
-        // Printing the score
-        LCD_Position(0,0);
+        // TODO: Increase the score over time (10 points per seconds)
+        // Printing the score on the second half of the LCD
+        LCD_Position(1,0);
         LCD_PrintNumber(*score);
 
+        // Button detection
+        /*
+         * Components name: SW1, SW2, SW3
+         */
+        if (SW1_Read()){
+           jump(&first_jump);
+        }
+        else if (SW2_Read()) {
+            duck();
+
+        } else if (SW3_Read()) {
+            // Reset the score
+            (*score) = 0;
+
+        }
+        // Light detection
+        detectLight(&photoResVal1, &photoResVal2, &day, &night);
+
+        // Keypad detection
+        detectKeyboard(&key);
+
+        // Shun-ting down  the LEDs
+        LED1_Write(0);
+        LED2_Write(0);
+        LED3_Write(0);
+        LED4_Write(0);
 
     }
 }
 
-// Reset the score
-/*
- * Components name : Button 3 = SW3
- */
-void resetScore(uint8_t* sw3_pressed, uint8_t* score){
-/* Assignment 3 : here we detect a rising edge on SW2 and display the number
-    of time SW2 has been pressed using ctr */
-
-    if (SW3_Read()){
-        *sw3_pressed = 1;
+// Jump function
+void jump(uint8_t* first_jump){
+    if (*first_jump){
+        (*first_jump) = 0;
     }
-    if ((!SW3_Read()) && *sw3_pressed){
-        *sw3_pressed = 0;
-        (*score) = 0;
-    }
+    // Printing on LCD
+    LCD_Position(0,0);
+    LCD_PrintString("JUMP");
+    // Lighting LEDs
+    LED1_Write(1);
+    LED2_Write(1);
 }
+// Down function
+void duck(){
+    // Printing on LCD
+    LCD_Position(0,0);
+    LCD_PrintString("DUCK");
+    // Lighting LEDs
+    LED3_Write(1);
+    LED4_Write(1);
+}
+
 
 // Keyboard detection
-void detectKeyboard(char* key, char* last_key){
+void detectKeyboard(char* key){
     // Detect the char pressed on the keybpard
     *key = keypadScan();
 
     if (*key != 'z'){
         if (*key == '2'){
-            // Call "JUMP" fuction
+            jump();
         }
         else if (*key == '8') {
-            // Call "DOWN" fuction
-
+            duck();
         }
     }
 }
 
+
 // Light detection
 /*
- * Components name : ADC = ADC
+ * Components name : ADC
  */
-void detectLight(uint32_t* photoResVal){
+void detectLight(uint32_t* photoResVal1, uint32_t* photoResVal2,uint8_t* day = 0, uint8_t* night = 0 ){
 
     Mux_Select(0);
     CyDelay(10); //This one let some time for the switch to occur. Otherwise conversion does not work properly
     ADC_StartConvert();
-
+    if(ADC_IsEndConversion(ADC_WAIT_FOR_RESULT)){
+        *photoResVal1 = ADC_GetResult32();
+    }
+    Mux_Select(1);
     CyDelay(10);
     ADC_StartConvert();
     if(ADC_IsEndConversion(ADC_WAIT_FOR_RESULT)){
-        *photoResVal = ADC_GetResult32();
+        *photoResVal2 = ADC_GetResult32();
     }
-    if(*photoResVal > 0 && *photoResVal <= 10000){
-        // BLACK
-        // Call "JUMP" fuction
+
+
+    // TODO: Calculate the speed delta
+    if(*photoResVal1 > 0 && *photoResVal1 <= 10000 && *photoResVal1 != *photoResVal2 ){
+        // Top: White, Bottom: Black
+        jump();
     }
-    else if(*photoResVal < 10000){
-        // WHITE
-        // Call "DOWN" fuction
+    else if(*photoResVal2 > 0 && *photoResVal2 <= 10000 && *photoResVal1 != *photoResVal2 ){
+        // Top: Black, Bottom: White
+        duck();
     }
+
+    // Checking the cycle of the day
+    if(*photoResVal2 > 10000 && (*photoResVal1 - *photoResVal2) <= 100 ){
+        // Top: White, Bottom: White
+        // Day
+        (*day) = 1;
+        (*night) = 0;
+
+    }
+    else if (*photoResVal1 > 0 && *photoResVal1 <= 10000 && (*photoResVal1 - *photoResVal2) <= 100) {
+        // Top: Black, Bottom: Black
+        // Night
+        (*day) = 0;
+        (*night) = 1;
+
+    }
+
 }
 
 
--- README.md
+++ README.md
@@ -2,23 +2,23 @@
 
 • Function "jump":
 - [ ] Fait fonctionner le servo moteur 1
-- [ ] Allume un LED 1&2
-- [ ] Ecrit "JUMP" sur le LCD
+- [x] Allume un LED 1&2
+- [x] Ecrit "JUMP" sur le LCD
 - [ ] Bruit audio type 1
 
 • Evolution du score avec le temps -> Accélération
-- [ ] Afficher le score sur le LCD
-
+- [x] Afficher le score sur le LCD
+- [ ] Augmenter le score avec le temps
 
 • Function "duck":
 - [ ] Fait fonctionner le servo moteur 2
-- [ ] Allume un LED 3&4
-- [ ] Ecrit "DUCK" sur le LCD
+- [x] Allume un LED 3&4
+- [x] Ecrit "DUCK" sur le LCD
 - [ ] Bruit audio type 2
 
 • for loop :
 - [ ] Détection avec photores -> call jump() or duck()
-- [ ] Détection button -> call jump() or duck()
+- [x] Détection button -> call jump() or duck()
 - [x] Détection keyboard -> call jump() or duck()
 - [ ] Détection serial port -> call jump() or duck()
 
