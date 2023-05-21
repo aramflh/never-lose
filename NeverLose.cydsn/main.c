edb17f628f8f947fe97b9ccc5703d30abd5534ea
Aramson <82621068+aramflh@users.noreply.github.com>
2023-05-21 16:11:54 +0200

    Beautify

--- NeverLose.cydsn/main.c
+++ NeverLose.cydsn/main.c
@@ -1,28 +1,48 @@
-//
-// Created by Aramson Felho on 10/05/2023.             
-//
+// TODO: Try to avoid global variables
 #include <math.h>
-
-#include "utils.h"   
-#include "project.h"
-#include "keypad.h"   
 #include <stdint.h>
+#include "project.h"
+#include "keypad.h"
 
-
+// Constants
 #define PI 3.14
 #define N 100
+const uint16_t START_DELAY = 600;
+const uint16_t PWM_PERIOD = 48000;
+const uint16_t MOTOR_HORIZ = 3000;
+const uint16_t MOTOR_PRESS = 2000;
+const uint32_t RES_VAL_1 = 28000; // A verifier
+const uint32_t RES_VAL_2 = 28000; // A verifier
 
-float signal1[N];
-float signal2[N];
-uint8 rxData;
-uint8_t sound1 = 0;
-uint8_t sound2 = 0;
-uint16_t i = 0;
-uint8_t value;
+// Global varialbes
+float g_signal1[N];
+float g_signal2[N];
+uint16_t g_soundCounter = 0;
 
 
-int obstacle_flag = 0;
-uint16 ms_count = 0;
+int jumpDelay = START_DELAY;
+int score = 0; // OK ? (voir ligne )
+uint8 rxData; // TODO
+
+
+uint8_t g_dac_value;
+uint8_t startGame = 0;
+uint8_t moved = 0;
+uint8_t cactus = 0;
+uint8_t mux_selector = 0;
+
+
+uint16_t scoreTimer = 0; // OK
+// iniStatesTimer delays the time before returning to the stable state after a movement
+uint16_t iniStatesTimer = 0; // OK
+uint16_t jumpTimer = 0; // OK
+uint16_t cnt5 = 0;
+uint32_t photoResVal1 = 0;
+uint32_t photoResVal2 = 0;
+uint32_t val_CMP;
+uint32_t val_adc;
+
+
 /*
 CY_ISR(isr_uart_Handler){
         uint8 status = 0;
@@ -32,9 +52,9 @@ CY_ISR(isr_uart_Handler){
                 rxData = UART_GetChar();
                 //UART_PutChar(rxData);
                 if (rxData == "jump") {
-                    LED1_Write(1);//jump(&first_jump, &move);
+                    LED1_Write(1);
                 }else if (rxData == "duck") {
-                    LED1_Write(0);//duck(&move);
+                    LED1_Write(0);
                 }
                 else{
                     LCD_Position(0,0);
@@ -45,295 +65,184 @@ CY_ISR(isr_uart_Handler){
 }*/
 
 
+/**
+ * Adds 10 to the value of score every second.
+ * The delay before jumping decrease of 100 every 100 points
+ * Formula: Area = PI*r^2
+ * @param[in] *scoreTimer
+ * @param[in] *score
+ */
+void start_score_counter(uint16_t *scoreTimer, int *score){
+    if (*scoreTimer < 1000){
+        *scoreTimer++;
+    } else {
+        *score += 10;
+        if(*score%100 == 0){
+            // /!\ delay is int => Possible to have neg number ?
+            // TODO: Change to uint or put a while
+            jumpDelay -= 100;
+        }
+        *scoreTimer = 0;
+    }
+}
+
+/**
+ * Deactivates the LED, clear the LCD display, put back the servo motor to their initial states
+ * and set the sound flags to '0' 200 milliseconds after un movement
+ * @param[in] *iniStatesTimer
+ */
+void initial_state(uint16_t *iniStatesTimer){
+        if (*iniStatesTimer < 200){
+            *iniStatesTimer++;
+        }
+        else {
+            LED1_Write(0);
+            LED2_Write(0);
+            LED3_Write(0);
+            LED4_Write(0);
+            LCD_ClearDisplay();
+            PWM1_WriteCompare(MOTOR_HORIZ);
+            PWM2_WriteCompare(MOTOR_HORIZ);
+            *iniStatesTimer = 0;
+            moved = 0 ;
+        }
+}
+
+/**
+ * Generates sound
+ * @param[in] *signal
+ */
+void make_sound(array signal){
+    if(0x80 & Timer_DAC_ReadStatusRegister()){
+        g_dac_value = 128 + 128*signal[*g_soundCounter];
+        DAC_SetValue(g_dac_value);
+        *g_soundCounter++;
+        if (*g_soundCounter==N){*g_soundCounter=0;}
+        Timer_DAC_ReadStatusRegister();
+    }
+}
+
+/**
+ * Prints "Jump" on the LCD display, lights LED1 & 2, generate sound and turn on the servo motor
+ * @param[in] *moved
+ */
+void jump(uint8_t *moved){
+
+    PWM1_WriteCompare(MOTOR_PRESS);
+    LCD_Position(0,0);
+    LCD_PrintString("Jump");
+    LED1_Write(1);
+    LED2_Write(1);
+    make_sound(g_signal1);
+    (*moved) = 1 ;
+}
+
+/**
+ * Prints "Duck" on the LCD display, lights LED3 & 24, generate sound and turn on the servo motor
+ * @param[in] *moved
+ */
+void duck(uint8_t *moved){
+
+    PWM2_WriteCompare(MOTOR_PRESS);
+    LCD_Position(0,0);
+    LCD_PrintString("Duck");
+    LED3_Write(1);
+    LED4_Write(1);
+    make_sound(g_signal2);
+    (*moved) = 1 ;
+}
+
+/**
+ * Prints "Duck" on the LCD display, lights LED3 & 24, generate sound and turn on the servo motor
+ * @param[in] *moved
+ */
+uint8_t objDetected(uint8_t mux_selector, uint32_t VAL){
+
+    Mux_Select(mux_selector);
+    // TODO: ???
+    //CyDelay(10);
+    ADC_StartConvert();
+    if(ADC_IsEndConversion(ADC_WAIT_FOR_RESULT)){
+        photoResVal = ADC_GetResult32();
+    }
+    if (photoResVal < VAL) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 int main(void)
 {
     CyGlobalIntEnable;  /* Enable global interrupts. */
 
-    /* Place your initialization/startup code*/
-    int score = 0;
-    uint8_t first_jump = 0;
-    uint8_t move = 0;
-    uint8_t delayMove = 0;
-    
-    
-    uint8_t day = 0;
-    uint8_t night = 0;
-    uint16_t pwm_period = 48000;
-    uint32_t photoResVal1 = 0;
-    uint32_t photoResVal2 = 0;
-    uint32_t val_CMP;
-    uint32_t val_adc;
-    char key = 'z';
-    uint16_t cnt1 = 0;
-    uint16_t cnt2 = 0;
-    uint16_t cnt3 = 0;
-    uint16_t cnt4 = 0;
-    uint16_t cnt5 = 0;
-    int delay = 600;
+    /* Variable declaration and initialization*/
 
-    PWM1_WritePeriod(pwm_period);
-    PWM2_WritePeriod(pwm_period);
-    //isr_uart_StartEx(isr_uart_Handler);
-
+    /*Startup code*/
 
     LCD_Start();
     LCD_ClearDisplay();
-    //Mux_Start();
-    ADC_Start();  // Start the Analog-to-Digital Converter
-    UART_Start(); //Start the UART
-    keypadInit();  // Call initialization function form keypad.h
-    PWM1_Start();
-    PWM2_Start();
-    PWM1_WriteCompare(3000); //Servo à l'horizontal
-    PWM2_WriteCompare(3000); //Servo à l'horizontal
-    
-    Timer_Start();
+    ADC_Start();
     DAC_Start();
     DAC_SetValue(0);
+    Mux_Start();
+    UART_Start();
+    keypadInit();
+    PWM1_WritePeriod(PWM_PERIOD);
+    PWM2_WritePeriod(PWM_PERIOD);
+    PWM1_Start();
+    PWM2_Start();
+    PWM1_WriteCompare(MOTOR_HORIZ);
+    PWM2_WriteCompare(MOTOR_HORIZ);
+    Timer_Start();
     Timer_DAC_Start();
     
-    //Son 1
+    // Interupt
+    //isr_uart_StartEx(isr_uart_Handler);
+    
+    //Sound
     for (int j=0; j<N; j++){
-        signal1[j] = sin(50*j/N);
+        g_signal1[j] = sin(50*j/N);
+        g_signal2[j] = sin(200*j/N);
     }
     
-    //Son 2
-    for (int j=0; j<N; j++){
-        signal2[j] = sin(200*j/N);
-    }
-
+    // LOOP
     for(;;)
     {
-        /* Place your application code here. */
+        LCD_Position(1,0);
+        LCD_PrintNumber(score);
+        // In case of overflow
+        if(0x80 & Timer_ReadStatusRegister()){
+            if (startGame) {
+                start_score_counter(&scoreTimer, &score);
+            }
+            if (moved) {
+                initial_state(*iniStatesTimer)
+            }
 
-        // TODO: Increase the score over time (10 points per seconds)
-       
-        if(0x80 & Timer_ReadStatusRegister()){ //In case of overflow
-            //Score +10 si on a sauté une fois
-            if (first_jump) {
-                if (cnt1 < 1000){
-                    cnt1++;
+            if(cactus){
+                if (jumpTimer < jumpDelay){
+                    jumpTimer++;
                 }
                 else{
-                    score += 10 ;
-                    if(score%100 == 0){
-                        delay -= 100;
-                    }
-                    cnt1 = 0; //Reset counter
+                    jump(&moved);
+                    jumpTimer = 0;
+                    cactus = 0;
                 }
             }
-            // Apres un mouvement , désactiver led, motor , son , lcd apres X secondes
-            if (move) {
-                // Led, LCD, 
-                if (cnt2 < 200){
-                    cnt2++;
-                }
-                else{
-                    move = 0 ;
-                    LED1_Write(0);
-                    LED2_Write(0);
-                    LED3_Write(0);
-                    LED4_Write(0);
-                    LCD_ClearDisplay();
-                    cnt2 = 0; //Reset counter
-                    sound1 = 0 ;
-                    sound2 = 0 ;
-                }
-                // Motor
-                if (cnt3 < 200){
-                    cnt3++;
-                }
-                else{
-                    PWM1_WriteCompare(3000);
-                    PWM2_WriteCompare(3000);
-                    cnt3 = 0; //Reset counter
-                }
-            }
-            if(delayMove){
-                if (cnt4 < delay){
-                    cnt4++;
-                }
-                else{
-                    jump(0, &move);
-                    cnt4 = 0; //Reset counter
-                    delayMove = 0;
-                }
+            if (keypadScan() == '2' || SW1_Read() || objDetected(0, RES_VAL_1) ) {
+                jump(&moved);
+            } else if (keypadScan() == '8' || SW2_Read() || || objDetected(1, RES_VAL_2)) {
+                duck(&moved);
+            } else if (SW3_Read()) {
+                LCD_ClearDisplay();
+                score = 0;
+                startGame = 0;
+                jumpDelay = START_DELAY;
             }
           
         }
-        
-        if(0x80 & Timer_DAC_ReadStatusRegister()){
-            if (sound1) {
-                value = 128 + 128*signal1[i];
-                DAC_SetValue(value);
-                i++;
-                if (i==N){i=0;}
-                Timer_DAC_ReadStatusRegister();
-            }
-            else if (sound2){
-                value = 128 + 128*signal2[i];
-                DAC_SetValue(value);
-                i++;
-                if (i==N){i=0;}
-                Timer_DAC_ReadStatusRegister();
-            }   
-        }
-        
-        // Printing the score on the second half of the LCD
-        LCD_Position(1,0);
-        LCD_PrintNumber(score);
 
-        // Button detection
-        /*
-         * Components name: SW1, SW2, SW3
-         */
-        if (SW1_Read()){
-           jump(&first_jump, &move);
-        }
-        else if (SW2_Read()) {
-            move = 1 ;
-            duck(&move);
-
-        } else if (SW3_Read()) {
-            // Reset the score
-            LCD_ClearDisplay();
-           score = 0;
-           delay = 600;
-           first_jump = 0;
-
-        }
-        //
-        
-        // Light detection
-        detectLight(&photoResVal1, &photoResVal2, &move, &delayMove);
-
-        // Keypad detection
-        detectKeyboard(&key,&first_jump, &move);   
-        
-
-    }
-}
-
-// Jump function
-void jump(uint8_t* first_jump,uint8_t* move ){
-    (*move) = 1 ;
-    if (first_jump){
-        (*first_jump) = 1;
-    }
-    //Activate servo motor 1
-    
-    // A Changer
-    
-    PWM1_WriteCompare(2000);
-    //CyDelay(200); 
-    //PWM1_WriteCompare(3000);
-     
-    // Printing "Jump" on LCD
-    LCD_Position(0,0);
-    LCD_PrintString("Jump");
-    // Lighting LEDs (1&2)
-    LED1_Write(1);
-    LED2_Write(1);
-    //CyDelay(500)       
-    // Audio Output 1
-    sound1 = 1 ;    
-    // TODO
-}
-// Down function
-void duck(uint8_t* move){
-    (*move) = 1 ;
-    //Activate servo motor 2
-    
-    // A Changer
-    
-    PWM2_WriteCompare(2000);
-    //CyDelay(200); 
-    //PWM2_WriteCompare(3000);
-    
-    // Printing "Duck" on LCD
-    LCD_Position(0,0);
-    LCD_PrintString("Duck");
-    // Lighting LEDs (3&4)
-    LED3_Write(1);
-    LED4_Write(1);
-    //CyDelay(200);
-    // Audio Output 2
-    sound2 = 1 ;
-    // TODO
-}
-
-// Keyboard detection
-
-void detectKeyboard(char* key,uint8_t* first_jump,uint8_t* move){
-    // Detect the char pressed on the keypadd
-    *key = keypadScan();
-
-    if (*key != 'z'){
-        if (*key == '2'){
-            jump(first_jump, move); 
-        }
-        else if (*key == '8') {
-            duck(move);
-        }
-    }
-}
-
-
-// Light detection
-/*
- * Components name : ADC
- */
-
-void detectLight(uint32_t* photoResVal1, uint32_t* photoResVal2, uint8_t* move, uint8_t* delayMove){
-/*
-    Mux_Select(0);
-    //CyDelay(10); //This one let some time for the switch to occur. Otherwise conversion does not work properly
-    ADC_StartConvert();
-    if(ADC_IsEndConversion(ADC_WAIT_FOR_RESULT)){
-        *photoResVal1 = ADC_GetResult32();
-    }
-   */
-    //Mux_Select(1);
-    //CyDelay(10);
-    ADC_StartConvert();
-    if(ADC_IsEndConversion(ADC_WAIT_FOR_RESULT)){
-        *photoResVal2 = ADC_GetResult32();
-    }
-
-
-    if(*photoResVal2 < 10000){
-        // Top: White, Bottom: Black
-        *delayMove = 1;
-    }
-    /*
-    else if(*photoResVal2 > 0 && *photoResVal2 <= COLOR_CHANGE && (*photoResVal1 - *photoResVal2) > COLOR_DELTA ){
-        // Top: Black, Bottom: White
-        duck(move);
-    }*/
-    
-    // Checking the cycle of the day
-    /*if(*photoResVal2 > COLOR_CHANGE && (*photoResVal1 - *photoResVal2) <= COLOR_DELTA ){
-        // Top: White, Bottom: White
-        // Day
-        //(*day) = 1;
-        //(*night) = 0;
-        // Printing the cycle on the LCD
-        LCD_Position(0,5);
-        LCD_PrintString("D");
-
-    }
-    else if (*photoResVal1 > 0 && *photoResVal1 <= COLOR_CHANGE && (*photoResVal1 - *photoResVal2) <= COLOR_DELTA) {
-        // Top: Black, Bottom: Black
-        // Night
-
-        //(*day) = 0;
-        //(*night) = 1;
-        // Printing the cycle on the LCD
-        LCD_Position(0,5);
-        LCD_PrintString("N");
-
-    }*/
 
 }
-
 
